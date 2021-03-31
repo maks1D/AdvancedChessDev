e141b5eb55a9368aba4093a980bddaa0782518e7
marximimus <40419155+marximimus@users.noreply.github.com>
2021-03-31 21:45:29 +0200

    linux build fix

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ clang src/captcha.c -c -o obj/captcha.o -std=gnu99
 clang src/chess.c -c -o obj/chess.o -std=gnu99
 clang src/database.c -c -o obj/database.o -std=gnu99
 clang src/server.c -c -o obj/server.o -std=gnu99
-clang obj/main.o obj/captcha.o obj/database.o obj/server.o -o AdvancedChess
+clang src/configuration.c -c -o obj/configuration.o -std=gnu99
+clang obj/main.o obj/captcha.o obj/database.o obj/server.o obj/configuration.o -o AdvancedChess
